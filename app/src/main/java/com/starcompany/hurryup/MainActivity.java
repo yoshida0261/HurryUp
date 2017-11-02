6214cf34633bba611456fd9371e8a75219b96708
yoshida0261 <yoshida261@gmail.com>
2017-11-02 13:12:10 +0900

    Annotation Processerを使用してボタンイベントを実装した

--- app/src/main/java/com/starcompany/hurryup/MainActivity.java
+++ app/src/main/java/com/starcompany/hurryup/MainActivity.java
@@ -1,13 +1,34 @@
 package com.starcompany.hurryup;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
+
+
+
+    @OnClick(R.id.buttonNum1)
+    public void Button(Button button)
+    {
+
+       // anser.setText("test");
+        Toast.makeText(this, "テスト", Toast.LENGTH_LONG).show();
+
+    }
+
 }
